1dc06691de5e37a0568c0b7011ae06e5f0e1e11e
IanLulu <ilulu2@uic.edu>
2023-09-20 16:31:16 -0500

    scrapped no animation collapsible, fixed collapsible button code

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
 var coll = document.getElementsByClassName("collapsible");
-var nacoll = document.getElementsByClassName("nacollapsible")
 var i;
-
-/* for simple collapsible (no animation) */
-for (i = 0; i < nacoll.length; i++) {
-    coll[i].addEventListener("click", function() { // anonymous function o.o its embarrassing i dunno how these work. i need to learn
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.display === "block") { // "===" is a strict equality comparison operator in JavaScript...
-            content.style.display = "none";
-        } else {
-            content.style.display = "block";
-        }
-    });
-}
 
 /* animated collapsible */
 for (i = 0; i < coll.length; i++) {
